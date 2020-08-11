de8bb9f9b3c91772afa5c76b183a5e67f8b7b186
panpf <panpfpanpf@outlook.com>
2020-08-11 17:02:31 +0800

    improve: The IndexedValue field is marked as final

--- tools4j-common/src/main/java/com/github/panpf/tools4j/common/IndexedValue.java
+++ tools4j-common/src/main/java/com/github/panpf/tools4j/common/IndexedValue.java
@@ -3,9 +3,9 @@ package com.github.panpf.tools4j.common;
 import org.jetbrains.annotations.NotNull;
 
 public class IndexedValue<T> {
-    public int index;
+    public final int index;
     @NotNull
-    public T value;
+    public final T value;
 
     public IndexedValue(int index, @NotNull T value) {
         this.index = index;
